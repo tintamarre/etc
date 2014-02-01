763e5f8d8cf883367ca67ea36e809ee3bf26949f
Zeb DeOs <zebdeos@bayprogrammer.com>
2014-01-31 20:26:29 -0800

    Remove emacs theme, but make gui work well with dark bg

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -25,7 +25,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
- '(custom-enabled-themes (quote (wombat)))
- '(frame-background-mode (quote dark)))
+ '(frame-background-mode (quote dark))
+)
 
