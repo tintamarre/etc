32afbb29ef5db83bd73b66f77c5acd23bfec961a
Zeb DeOs <zebdeos@coquilletribe.org>
2014-09-05 16:34:55 -0700

    Customize emacs

--- dotfiles/emacs.d/init.el
+++ dotfiles/emacs.d/init.el
@@ -7,10 +7,10 @@
               inhibit-startup-screen t
               display-time-24hr-format 1)
 
-(add-to-list 'load-path "~/.emacs.d/lisp/evil")
-(require 'evil)
-(evil-mode 1)
-(setq evil-default-cursor t)
+; (add-to-list 'load-path "~/.emacs.d/lisp/evil")
+; (require 'evil)
+; (evil-mode 1)
+; (setq evil-default-cursor t)
 
 ;; http://orgmode.org/manual/Activation.html#Activation
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
@@ -25,8 +25,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- ;; '(custom-enabled-themes (quote (tango-dark)))
- '(frame-background-mode (quote dark)))
+ '(custom-enabled-themes (quote (wombat)))
+ '(frame-background-mode (quote dark))
+ '(indicate-empty-lines t)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil)
+ '(truncate-lines t))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
