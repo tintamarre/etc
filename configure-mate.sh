2eee8a35434391c1c2bba78ba1a299d09a095031
Zeb DeOs <zebdeos@bayprogrammer.com>
2014-12-13 16:09:40 -0800

    Write script to set dconf values for mate

---
+++ configure-mate.sh
@@ -0,0 +1,7 @@
+dconf write /org/mate/desktop/session/required-components/windowmanager "'fluxbox'"
+dconf write /org/mate/desktop/peripherals/keyboard/delay 160
+dconf write /org/mate/desktop/peripherals/keyboard/numlock-state "'on'"
+dconf write /org/mate/desktop/peripherals/keyboard/rate 100
+dconf write /org/mate/desktop/peripherals/keyboard/kbd/layouts "['us', 'us\tcolemak']"
+dconf write /org/mate/desktop/peripherals/keyboard/kbd/options "['caps\tcaps:escape']"
+dconf write /org/mate/desktop/session/idle-delay 30
