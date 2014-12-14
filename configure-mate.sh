96b1b0f3fcc3f80790726c62aeaaed72907c565d
Zeb DeOs <zebdeos@bayprogrammer.com>
2014-12-13 16:29:15 -0800

    Put mate workspaces on two rows

--- configure-mate.sh
+++ configure-mate.sh
@@ -5,3 +5,4 @@ dconf write /org/mate/desktop/peripherals/keyboard/rate 100
 dconf write /org/mate/desktop/peripherals/keyboard/kbd/layouts "['us', 'us\tcolemak']"
 dconf write /org/mate/desktop/peripherals/keyboard/kbd/options "['caps\tcaps:escape']"
 dconf write /org/mate/desktop/session/idle-delay 30
+dconf write /org/mate/panel/objects/workspace-switcher/prefs/num-rows 2
