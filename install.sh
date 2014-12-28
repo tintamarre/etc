03ae9d872ec0c7c531e049971b7b6981d108888b
Zeb DeOs <zebdeos@bayprogrammer.com>
2014-12-27 16:31:25 -0800

    Change location of dotfiles

--- install.sh
+++ install.sh
@@ -14,49 +14,49 @@
 [ -e $HOME/.sbclrc -o -L $HOME/.sbclrc ] && rm $HOME/.sbclrc
 [ -e $HOME/.fluxbox -o -L $HOME/.fluxbox ] && rm -rf $HOME/.fluxbox
 
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/vimrc $HOME/.vimrc
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/gvimrc $HOME/.gvimrc
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/vim $HOME/.vim
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/emacs.d $HOME/.emacs.d
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/gitconfig $HOME/.gitconfig
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/hgrc $HOME/.hgrc
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/jrnl_config $HOME/.jrnl_config
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/profile $HOME/.profile
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/zshrc $HOME/.zshrc
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/terminfo $HOME/.terminfo
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/sbclrc $HOME/.sbclrc
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/fluxbox $HOME/.fluxbox
+ln -s $HOME/etc/public/vimrc $HOME/.vimrc
+ln -s $HOME/etc/public/gvimrc $HOME/.gvimrc
+ln -s $HOME/etc/public/vim $HOME/.vim
+ln -s $HOME/etc/public/emacs.d $HOME/.emacs.d
+ln -s $HOME/etc/public/gitconfig $HOME/.gitconfig
+ln -s $HOME/etc/public/hgrc $HOME/.hgrc
+ln -s $HOME/etc/public/tmux.conf $HOME/.tmux.conf
+ln -s $HOME/etc/public/jrnl_config $HOME/.jrnl_config
+ln -s $HOME/etc/public/profile $HOME/.profile
+ln -s $HOME/etc/public/zshrc $HOME/.zshrc
+ln -s $HOME/etc/public/terminfo $HOME/.terminfo
+ln -s $HOME/etc/public/sbclrc $HOME/.sbclrc
+ln -s $HOME/etc/public/fluxbox $HOME/.fluxbox
 
 [ -d $HOME/.ssh ] || mkdir $HOME/.ssh
 [ -e $HOME/.ssh/authorized_keys -o -L $HOME/.ssh/authorized_keys ] && rm $HOME/.ssh/authorized_keys
-[ -e $HOME/gandalf/src/private/dotfiles-private/ssh/authorized_keys ] && \
-    cp -p $HOME/gandalf/src/private/dotfiles-private/ssh/authorized_keys \
+[ -e $HOME/etc/private/ssh/authorized_keys ] && \
+    cp -p $HOME/etc/private/ssh/authorized_keys \
         $HOME/.ssh/authorized_keys
 [ -e $HOME/.ssh/id_rsa -o -L $HOME/.ssh/id_rsa ] && rm $HOME/.ssh/id_rsa
-[ -e $HOME/gandalf/src/private/dotfiles-private/ssh/id_rsa  ] && \
-    cp -p $HOME/gandalf/src/private/dotfiles-private/ssh/id_rsa \
+[ -e $HOME/etc/private/ssh/id_rsa  ] && \
+    cp -p $HOME/etc/private/ssh/id_rsa \
         $HOME/.ssh/id_rsa
 [ -e $HOME/.ssh/id_rsa.pub -o -L $HOME/.ssh/id_rsa.pub ] && rm $HOME/.ssh/id_rsa.pub
-[ -e $HOME/gandalf/src/private/dotfiles-private/ssh/id_rsa.pub ] && \
-    cp -p $HOME/gandalf/src/private/dotfiles-private/ssh/id_rsa.pub \
+[ -e $HOME/etc/private/ssh/id_rsa.pub ] && \
+    cp -p $HOME/etc/private/ssh/id_rsa.pub \
         $HOME/.ssh/id_rsa.pub
 
-[ -d $HOME/finglas ] || mkdir $HOME/finglas
-[ -d $HOME/finglas/tmp ] || mkdir $HOME/finglas/tmp
-[ -d $HOME/finglas/bin ] || mkdir $HOME/finglas/bin
-[ -d $HOME/finglas/xdg-dirs ] || mkdir $HOME/finglas/xdg-dirs
-[ -d $HOME/finglas/xdg-dirs/Desktop ] || mkdir $HOME/finglas/xdg-dirs/Desktop
-[ -d $HOME/finglas/xdg-dirs/Documents ] || mkdir $HOME/finglas/xdg-dirs/Documents
-[ -d $HOME/finglas/xdg-dirs/Downloads ] || mkdir $HOME/finglas/xdg-dirs/Downloads
-[ -d $HOME/finglas/xdg-dirs/Music ] || mkdir $HOME/finglas/xdg-dirs/Music
-[ -d $HOME/finglas/xdg-dirs/Pictures ] || mkdir $HOME/finglas/xdg-dirs/Pictures
-[ -d $HOME/finglas/xdg-dirs/Public ] || mkdir $HOME/finglas/xdg-dirs/Public
-[ -d $HOME/finglas/xdg-dirs/Templates ] || mkdir $HOME/finglas/xdg-dirs/Templates
-[ -d $HOME/finglas/xdg-dirs/Videos ] || mkdir $HOME/finglas/xdg-dirs/Videos
+[ -d $HOME/local ] || mkdir $HOME/local
+[ -d $HOME/local/tmp ] || mkdir $HOME/local/tmp
+[ -d $HOME/local/bin ] || mkdir $HOME/local/bin
+[ -d $HOME/local/xdg-dirs ] || mkdir $HOME/local/xdg-dirs
+[ -d $HOME/local/xdg-dirs/Desktop ] || mkdir $HOME/local/xdg-dirs/Desktop
+[ -d $HOME/local/xdg-dirs/Documents ] || mkdir $HOME/local/xdg-dirs/Documents
+[ -d $HOME/local/xdg-dirs/Downloads ] || mkdir $HOME/local/xdg-dirs/Downloads
+[ -d $HOME/local/xdg-dirs/Music ] || mkdir $HOME/local/xdg-dirs/Music
+[ -d $HOME/local/xdg-dirs/Pictures ] || mkdir $HOME/local/xdg-dirs/Pictures
+[ -d $HOME/local/xdg-dirs/Public ] || mkdir $HOME/local/xdg-dirs/Public
+[ -d $HOME/local/xdg-dirs/Templates ] || mkdir $HOME/local/xdg-dirs/Templates
+[ -d $HOME/local/xdg-dirs/Videos ] || mkdir $HOME/local/xdg-dirs/Videos
 
 [ -d $HOME/.config ] || mkdir $HOME/.config
 [ -e $HOME/.config/user-dirs.dirs -o -L $HOME/.config/user-dirs.dirs ] && \
     rm $HOME/.config/user-dirs.dirs
-ln -s $HOME/gandalf/src/github/bayprogrammer/dotfiles/dotfiles/config/user-dirs.dirs \
+ln -s $HOME/etc/public/config/user-dirs.dirs \
     $HOME/.config/user-dirs.dirs
